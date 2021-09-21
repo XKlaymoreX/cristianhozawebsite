b60fab34a8da99fe2a9373d33e1de33373af9ef3
System Administrator <root@MacBook-Pro-di-Cristian.local>
2021-09-21 15:24:20 +0200

    Optimized image

--- out/_next/static/8JHolMK2sbf99bx9Ai1yp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8ceb834a1698f8c43b57.css","static/chunks/pages/index-f757958ef868dabfb8c8.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
